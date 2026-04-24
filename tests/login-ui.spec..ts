391a4d4c106996e8b952d1e1b14329729e6a6662
dalini-ts <dalini.ts@gmail.com>
2026-04-24 14:54:02 +0530

    groups added smoke

--- tests/login-ui.spec..ts
+++ tests/login-ui.spec..ts
@@ -2,17 +2,17 @@ import { test, expect } from '../fixtures/base.js';
 
 test.describe("OrangeHRM Login UI Tests", () => {
 
-  test('verify title', async ({ page }) => {
+  test('verify title',{ tag: '@regression',}, async ({ page }) => {
     await expect(page).toHaveTitle('OrangeHRM')
   });
 
   //test for header, placeholder
-  test('verify header', async ({ page }) => {
+  test('verify header', { tag: '@regression',}, async ({ page }) => {
     await expect(page.locator("xpath=//h5[text()='Login']")).toHaveText('Login')
   });
 
   //verify placeholder of username and password
-  test('verify placeholder', async ({ page }) => {
+  test('verify placeholder',{ tag: '@smoke',}, async ({ page }) => {
     await expect.soft(page.locator("xpath=//input[@name='username']")).toHaveAttribute('placeholder', 'Username')
     await expect.soft(page.locator("xpath=//input[@name='password']")).toHaveAttribute('placeholder', 'Password')
   })
