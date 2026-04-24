e293279bda979af1eecfe8bc9125a228ca1fc1e4
dalini-ts <dalini.ts@gmail.com>
2026-04-24 15:42:36 +0530

    keywords added

--- tests/login2.spec.ts
+++ tests/login2.spec.ts
@@ -1,43 +1,32 @@
 import { test, expect } from '../fixtures/base.js';
 import { invalidLoginData } from '../utils/data-source.js'
-import { JsonUtils } from '../utils/json-utlis.js'
-import { ExcelUtils } from '../utils/excel-utils.js'
+import { JsonUtils } from '../utils/json-utils.js'
+import { LoginPage } from '../pages/LoginPage.js';
+import { DashboardPage } from '../pages/DashboardPage.js'
 
 test.describe("OrangeHRM Login Function  Tests", () => {
 
   for (const { username, password, expected_error } of invalidLoginData) {
     test(`verify invalid login: ${username} and ${password}`, async ({ page }) => {
-      await page.locator("xpath=//input[@name='username']").fill(username)
-      await page.locator("xpath=//input[@name='password']").fill(password)
-      await page.locator("xpath=//button[contains(normalize-space(),'Login')]").click()
-      await expect(page.locator("xpath=//p[text()='Invalid credentials']")).toHaveText(expected_error)
-    });
-  }
-
-  for (const { username, password, expected_error } of JsonUtils.getJson('invalidLoginData')) {
-    test(`verify invalid login from json: ${username} and ${password}`, async ({ page }) => {
-      await page.locator("xpath=//input[@name='username']").fill(username)
-      await page.locator("xpath=//input[@name='password']").fill(password)
-      await page.locator("xpath=//button[contains(normalize-space(),'Login')]").click()
-      await expect(page.locator("xpath=//p[text()='Invalid credentials']")).toHaveText(expected_error)
+      const loginPage = new LoginPage(page);
+      await loginPage.fillUsername(username);
+      await loginPage.fillPassword(password);
+      await loginPage.clickLogin();
+      await loginPage.verifyInvalidErrorMessage(expected_error);
     });
   }
 
   for (const { username, password, expected_value } of JsonUtils.getJson('validLoginData')) {
     test(`verify valid login: ${username} and ${password}`, async ({ page }) => {
-      await page.locator("xpath=//input[@name='username']").fill(username)
-      await page.locator("xpath=//input[@name='password']").fill(password)
-      await page.locator("xpath=//button[contains(normalize-space(),'Login')]").click()
-      await expect(page.locator("xpath=//h6[text()='Dashboard']")).toHaveText(expected_value)
-    });
-  }
+      const loginPage = new LoginPage(page);
+      await loginPage.fillUsername(username);
+      await loginPage.fillPassword(password);
+      await loginPage.clickLogin();
 
-  for (const { username, password, expected_error } of ExcelUtils.getSheetToJson('invalidLoginData')) {
-    test(`verify invalid login from excel: ${username} and ${password}`, async ({ page }) => {
-      await page.locator("xpath=//input[@name='username']").fill(username)
-      await page.locator("xpath=//input[@name='password']").fill(password)
-      await page.locator("xpath=//button[contains(normalize-space(),'Login')]").click()
-      await expect(page.locator("xpath=//p[text()='Invalid credentials']")).toHaveText(expected_error)
+      const dashboardPage = new DashboardPage(page);
+      await dashboardPage.verifyDashboardHeader(expected_value);
     });
   }
-})
+});
+
+// npx playwright test --grep "(?=.*@smoke)(?=.*@regression)"
